b19241dabb70f921d5b611ce75b500910e15ce35
Sanders Lauture <golf1052@msn.com>
2024-04-14 22:27:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# golf1052.ThreadsAPI
+
+[Threads Docs](https://developers.facebook.com/docs/threads)
---
+++ golf1052.ThreadsAPI/ThreadsClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace golf1052.ThreadsAPI
+{
+    public class ThreadsClient
+    {
+    }
+}
